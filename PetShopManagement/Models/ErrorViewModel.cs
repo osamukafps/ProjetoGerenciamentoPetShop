d0465098d9c0c1eefb742fe59a4bcb5d298091e5
Samuel Hipólito <samuelhplt@gmail.com>
2022-03-31 20:12:24 -0300

    Projeto criado

---
+++ PetShopManagement/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PetShopManagement.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

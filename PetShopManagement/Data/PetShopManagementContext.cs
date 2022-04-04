2541162c1fce8de2848026621b42444165efaad5
Samuel Hipólito <samuelhplt@gmail.com>
2022-04-04 20:06:27 -0300

    Implementação do CRUD para os Serviços

---
+++ PetShopManagement/Data/PetShopManagementContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using PetShopManagement.Models;
+
+namespace PetShopManagement.Data
+{
+    public class PetShopManagementContext : DbContext
+    {
+        public PetShopManagementContext (DbContextOptions<PetShopManagementContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Servicos> Servicos { get; set; }
+    }
+}
---
+++ PetShopManagement/Models/Servicos.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PetShopManagement.Models
+{
+    public class Servicos
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public double Valor { get; set; }
+
+    }
+}
